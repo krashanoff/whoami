a6321832208fd3334322485db821455865178ba1
krashanoff <lkrashanoff2@gmail.com>
2016-05-03 17:12:53 -0700

    Add a bunch of files, organize, and make a preliminary chart

---
+++ main.js
@@ -0,0 +1,1 @@
+var ctx=$("#myChart");var myChart=new Chart(ctx,{type:"bar",data:{labels:["Red","Blue","Yellow","Green","Purple","Orange"],datasets:[{label:"# of Votes",data:[12,19,3,5,2,3]}]},options:{scales:{yAxes:[{ticks:{beginAtZero:true}}]}}});
