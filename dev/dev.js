4748711372a624cdb7c9bc290d2d9fc90a618e09
krashanoff <lkrashanoff2@gmail.com>
2016-05-04 17:41:01 -0700

    Lower number of particles

--- dev/dev.js
+++ dev/dev.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
   particlesJS("particles-js", {
     "particles": {
       "number": {
-        "value": 50,
+        "value": 30,
         "density": {
           "enable": true,
           "value_area": 800
